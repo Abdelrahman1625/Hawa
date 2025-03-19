7f5ee5ba0770a052608cc2b9f6730956dc7d5ef6
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-03-20 01:33:26 +0200

    add routes of chat

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import http from "http";
 
 //import { auth, authorize } from "./api/middlewares/auth.js";
 import connectDB from "./config/database.js";
@@ -12,10 +13,14 @@ import rideRoutes from "./api/routes/rideRoutes.js";
 import paymentRoutes from "./api/routes/paymentRoutes.js";
 import reviewRoutes from "./api/routes/reviewRoutes.js";
 import adminRoutes from "./api/routes/adminRoutes.js";
+import chatRoutes from "./api/routes/chatRoutes.js"
+import { initWebSocket } from "./services/socket/chatSocket.js";
 
 dotenv.config();
 
 const app = express();
+// Create HTTP server
+const server = http.createServer(app)
 
 // Middleware to parse cookies
 app.use(cookieParser());
@@ -35,11 +40,15 @@ app.use("/api/rides", rideRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/chats", chatRoutes);
 
 // Protected Routes
 //app.use('/api/admin', auth, authorize('admin'), adminRoutes);
 //app.use('/api/rides', auth, rideRoutes);
 //app.use('/api/payments', auth, paymentRoutes);
+
+// Initialize WebSocket server
+const wss = initWebSocket(server)
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
